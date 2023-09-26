952d2a82e321f1e679143bf26a6fce394d1b038b
Onur Alp Bicer <onuralpbicer@gmail.com>
2023-09-26 17:53:54 +1000

    Sync page visuals

--- src/app/sync-page/sync-page.component.ts
+++ src/app/sync-page/sync-page.component.ts
@@ -2,21 +2,32 @@ import { Component, OnInit } from '@angular/core'
 import { Store } from '@ngrx/store'
 import { ISyncState, ISyncStatus, syncFeature } from '../sync/sync.reducer'
 import { SyncActions } from '../sync/sync.actions'
-import { Observable } from 'rxjs'
+import { Observable, map } from 'rxjs'
 import { CommonModule } from '@angular/common'
+import { IonicModule } from '@ionic/angular'
+import { includes } from 'rambda'
 
 @Component({
     selector: 'app-sync-page',
     templateUrl: './sync-page.component.html',
     styleUrls: ['./sync-page.component.scss'],
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, IonicModule],
 })
 export class SyncPageComponent implements OnInit {
     public syncStatus$!: Observable<ISyncStatus>
+    public isLoading$!: Observable<boolean>
+
+    public SyncStatus = ISyncStatus
 
     constructor(private syncStore: Store<ISyncState>) {
         this.syncStatus$ = this.syncStore.select(syncFeature.selectViewStatus)
+
+        this.isLoading$ = this.syncStatus$.pipe(
+            map((status) =>
+                includes(status, [ISyncStatus.Checking, ISyncStatus.Syncing]),
+            ),
+        )
     }
 
     ngOnInit(): void {
