e0058cd346d557c4853ca3dce1efb0c4828e5858
Onur Alp Bicer <onuralpbicer@gmail.com>
2023-09-27 17:45:21 +1000

    Login button animation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,16 +3,19 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 import { ViewStatus } from '../shared/types'
 import { AuthService } from '../shared/auth.service'
 import { IonicModule } from '@ionic/angular'
+import { CommonModule } from '@angular/common'
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
     standalone: true,
-    imports: [IonicModule, ReactiveFormsModule],
+    imports: [CommonModule, IonicModule, ReactiveFormsModule],
 })
 export class LoginComponent implements OnInit {
     public viewStatus = ViewStatus.INITIAL
+
+    public readonly ViewStatus = ViewStatus
 
     constructor(private fb: FormBuilder, private authService: AuthService) {}
 
